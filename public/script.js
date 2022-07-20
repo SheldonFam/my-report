766bc2aa484060d7815c620bd81f167492df077c
Sheldon Fam <sheldonfam95@gmail.com>
2022-07-20 12:37:16 +0800

    update button function

--- public/script.js
+++ public/script.js
@@ -23,15 +23,15 @@ const ui = {
     reports.forEach((report) => {
       tasksList.innerHTML += `
      <div id=${report._id}>
-          <span class="fw-bold">${report.reportNo}</span>
-          <span class="small text-secondary">${report.date}</span>
-          <p>${report.customerName}</p>
-          <p>${report.jobscope}</p>
-          <p>${report.countermeasure}</p>
-          <span class="options">
-          <i  id="edit-report" data-bs-toggle="modal" data-bs-target="#form" class="fas fa-edit"></i>
-          <i  id="delete-report" class="fas fa-trash-alt"></i>
-          <button onClick="openReport()" class="view_report"  id="view-report" >View</button>
+      <span class="fw-bold">${report.reportNo}</span>
+      <span class="small text-secondary">${report.date}</span>
+      <p>${report.customerName}</p>
+      <p>${report.jobscope}</p>
+      <p>${report.countermeasure}</p>
+      <span class="options">
+          <button onClick="editReport(event)" class="btn-edit" data-bs-toggle="modal" data-bs-target="#form"><i class="fas fa-edit"></i></button>
+          <button onClick="deleteReport(event)" class="btn-delete"  ><i class="fas fa-trash-alt"></i></button>
+          <button onClick="viewReport(event)" class="btn-view">View</button>
           </span>
     </div>
     `;
@@ -50,16 +50,14 @@ const jobInput = document.getElementById("jobInput");
 const actionInput = document.getElementById("actionInput");
 const msg = document.getElementById("msg");
 const tasksList = document.getElementById("tasks");
-const addButton = document.getElementById("add_button"); // not use??
-const saveButton = document.getElementById("save_button");
+const addButton = document.getElementById("btn-add");
 
 //Setup events
 //Event Listeners
 //When user press add button after submit data
-form.addEventListener("submit", (events) => {
-  events.preventDefault();
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
   formValidation();
-
   resetForm();
 });
 
@@ -103,53 +101,45 @@ const acceptData = (onSuccess) => {
     });
 };
 
-// Inside the small task , icon and function
-tasksList.addEventListener("click", (e) => {
-  e.preventDefault();
+function editReport(event) {
+  $("#btn-add").hide();
+  $(".btn-save").show();
+  let selecteditem = event.target.parentElement.parentElement;
+  reportInput.value = selecteditem.children[0].innerHTML;
+  dateInput.value = selecteditem.children[1].innerHTML;
+  customerInput.value = selecteditem.children[2].innerHTML;
+  jobInput.value = selecteditem.children[3].innerHTML;
+  actionInput.value = selecteditem.children[4].innerHTML;
+}
 
-  let editButtonIsPressed = e.target.id == "edit-report";
+function deleteReport(event) {
+  let id = event.target.parentElement.parentElement.id;
+  fetch(`${url}/${id}`, {
+    method: "DELETE",
+  })
+    .then((res) => res.json())
+    .then(() => loadReportList());
+}
 
-  let deleteButtonIsPressed = e.target.id == "delete-report";
+function viewReport(event) {
+  reportModal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+  let id = event.target.parentElement.parentElement.id;
+  fetch(`${url}/${id}`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then((res) => res.json())
+    .then((data) => showData(data.report));
+}
 
-  let viewReportButton = e.target.id == "view-report";
-
-  let id = e.target.parentElement.parentElement.id;
-
-  //Delete-Remove the existing post
-  //Method:DELETE
-  if (deleteButtonIsPressed) {
-    fetch(`${url}/${id}`, {
-      method: "DELETE",
-    })
-      .then((res) => res.json())
-      .then(() => loadReportList());
-  }
-
-  if (editButtonIsPressed) {
-    $("#add_button").hide();
-    $("#save_button").show();
-    let selectedTask = e.target.parentElement.parentElement;
-    reportInput.value = selectedTask.children[0].innerHTML;
-    dateInput.value = selectedTask.children[1].innerHTML;
-    customerInput.value = selectedTask.children[2].innerHTML;
-    jobInput.value = selectedTask.children[3].innerHTML;
-    actionInput.value = selectedTask.children[4].innerHTML;
-  }
-
-  if (viewReportButton) {
-    fetch(`${url}/${id}`, {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    })
-      .then((res) => res.json())
-      .then((data) => showData(data.report));
-
-    const showData = (report) => {
-      const viewReport = document.querySelector(".report");
-      viewReport.innerHTML = `
+const showData = (report) => {
+  const viewReport = document.querySelector(".report");
+  viewReport.innerHTML = `
         <table class="table"  ${report._id}  >
+        <h3>${report.reportNo}</h3>
         <thead>
           <tr>
             <th>Report Date</th>
@@ -169,43 +159,45 @@ tasksList.addEventListener("click", (e) => {
         </table>
           <button onClick="closeReport()" class="close-report" >&times;</button>
      `;
-    };
-  }
+};
 
-  //Update
-  //Method:PATCH
-  saveButton.addEventListener("click", (events) => {
-    events.preventDefault();
-    fetch(`${url}/${id}`, {
-      method: "PATCH",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        reportNo: reportInput.value,
-        date: dateInput.value,
-        customerName: customerInput.value,
-        jobscope: jobInput.value,
-        countermeasure: actionInput.value,
-      }),
-    })
-      .then((res) => res.json())
-      .then(() => location.reload());
-    $("#form").modal("hide");
-  });
-});
+//Update
+//Method:PATCH
+const saveButton = document.querySelector(".btn-save");
+console.log(saveButton);
+
+function saveReport() {
+  console.log("save");
+}
+
+// fetch(`${url}/${id}`, {
+//   method: "PATCH",
+//   headers: {
+//     "Content-Type": "application/json",
+//   },
+//   body: JSON.stringify({
+//     reportNo: reportInput.value,
+//     date: dateInput.value,
+//     customerName: customerInput.value,
+//     jobscope: jobInput.value,
+//     countermeasure: actionInput.value,
+//   }),
+// })
+//   .then((res) => res.json())
+//   .then(() => location.reload());
+// $("#form").modal("hide");
 
 //After submit data, dismiss modal and show addbutton
 form.addEventListener("hidden.bs.modal", function () {
   resetForm();
   msg.innerHTML = " ";
-  $("#add_button").show();
-  $("#save_button").hide();
+  $("#btn-add").show();
+  $(".btn-save").hide();
 });
 
 //When create new report is pressed, hide save button
 form.addEventListener("show.bs.modal", function () {
-  $("#save_button").hide();
+  $(".btn-save").hide();
 });
 
 //Reset the form
@@ -221,11 +213,6 @@ const resetForm = () => {
 //When viewButton is pressed
 const reportModal = document.querySelector(".report");
 const overlay = document.querySelector(".overlay");
-
-const openReport = () => {
-  reportModal.classList.remove("hidden");
-  overlay.classList.remove("hidden");
-};
 
 //Close viewReport
 const closeReport = () => {
